c3e1e3e8ab2cb3af6314a7dcabf9ba9fb642b5c5
431059 <syn201012@163.com>
2019-10-13 17:55:04 +0800

    vue-management-system

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about container">
+    <h1 class="page-header">希望能够提供给大家参考!</h1>
+    <p>syn2015</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
